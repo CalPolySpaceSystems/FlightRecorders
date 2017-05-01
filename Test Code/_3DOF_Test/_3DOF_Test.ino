40a2836f8ec34f5a40861d21af69603cb4682cd4
Patrick Chizek <patrick.chizek@comcast.net>
2017-04-30 17:04:23 -0700

    Worked out data acquisition

--- Test Code/_3DOF_Test/_3DOF_Test.ino
+++ Test Code/_3DOF_Test/_3DOF_Test.ino
@@ -2,8 +2,7 @@
 #include <Wire.h>
 
 // Address
-const byte A3G_R_ADD = 0b1101001;
-const byte A3G_W_ADD = 0b1101001;
+#define A3G_DEVICE_ADD (0b1101001)
 
 //CTRL REGS
 const byte A3G_CTRL_REG1 = 0x20;
@@ -13,17 +12,15 @@ const byte A3G_CTRL_REG4 = 0x23;
 const byte A3G_CTRL_REG5 = 0x24;
 
 // END REgs
-const byte A3G_OUT_TEMP = 0x26;
-const byte A3G_STATUS_REG = 0x27;
-const byte A3G_OUT_START = 0x28;
+#define A3G_OUT_TEMP   (0x26)
+#define A3G_STATUS_REG (0x27)
+#define A3G_OUT_X (0x28)
+#define A3G_OUT_Y (0x2A)
+#define A3G_OUT_Z (0x2C)
+#define STAT_ZYXOR (0x80)
 
-typedef union Sense
-{
-  float num[3];
-  byte byt[12];
-}sense;
+double A3GData[3] = {0};
 
-sense gyr;
 String dataString;
 
 
@@ -32,41 +29,26 @@ void setup() {
   SerialUSB.begin(9600);
 
   Wire.begin();
-  writeReg(A3G_CTRL_REG1,0b00001111,A3G_W_ADD); //Enable Gyro, Set Bandwidth
+  writeReg(A3G_CTRL_REG1,0b00111111,A3G_DEVICE_ADD); //Enable Gyro, Set Bandwidth
+  writeReg(A3G_CTRL_REG2,0b00000011,A3G_DEVICE_ADD);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
   
-  readData(gyr.num,A3G_R_ADD,A3G_OUT_START);
-
-  for (byte i=0;i<3;i++){
-    gyr.num[i] *= .001;
-  }
-
-  dataString = (String(gyr.num[0]) + ',' + String(gyr.num[1]) + ',' + String(gyr.num[2]) + ',');
+  A3GData[0] = readA3G('x');
+  A3GData[1] = readA3G('y');
+  A3GData[2] = readA3G('z');
+ 
+  dataString = (String(A3GData[0]) + ", " + String(A3GData[1]) + ", " + String(A3GData[2]) + ';');
   SerialUSB.println(dataString);
-    
-}
 
-/*
-void readA3G(float data[], byte deviceAddress, byte dataAddress{
-  
-  for (byte i=0;i<9;i++){
-    Wire.beginTransmission(deviceAddress);
-    Wire.write(dataAddress);
-    Wire.endTransmission(false);
-    
-    Wire.requestFrom(deviceAddress, 1,true);
-    while(Wire.available() == 0){}  
-
-    data[i] = Wire.read()
-     
-    startRegister += 0b1;
+  if (checkRegister(A3G_STATUS_REG,STAT_ZYXOR,A3G_DEVICE_ADD)){
+    SerialUSB.println("An overwrite occured!");
   }
-    
+  
 }
-*/
 
 //This function takes a value and writes it to a given register at a given address
 void writeReg(byte reg, byte value, byte device){
@@ -76,16 +58,52 @@ void writeReg(byte reg, byte value, byte device){
   Wire.endTransmission();
 }
 
-void readData(float data[], byte devAdd, byte startReg){
-  Wire.beginTransmission(devAdd);
-  Wire.write(startReg);
+double readA3G(char axis){
+  int16_t rawData;
+  for (byte i=0;i<2;i++){
+   
+    Wire.beginTransmission(A3G_DEVICE_ADD);   
+    switch(axis){
+      case 'x':  
+        Wire.write(A3G_OUT_X + (1-i));
+      case 'y': 
+        Wire.write(A3G_OUT_Y + (1-i));
+      case 'z':
+        Wire.write(A3G_OUT_Z + (1-i));
+      default:
+        break;
+    }
+    Wire.endTransmission(false);
+    
+    Wire.requestFrom(A3G_DEVICE_ADD,1,true);
+    while (Wire.available() == 0);
+    
+    rawData |= (Wire.read() << 8*(1-i));
+    
+  }
+
+  //(rawData * 0.00875f); 
+  //out *= 0.00875;
+  return ((double)rawData * 0.007477);
+}
+
+bool checkRegister(byte targetRegister, byte expectedValue, byte deviceAddress){
+  Wire.beginTransmission(deviceAddress);
+  Wire.write(targetRegister);
   Wire.endTransmission(false);
-  Wire.requestFrom(devAdd,6,true);
-  while (Wire.available() == 0){
+
+  Wire.requestFrom(deviceAddress,1,true);
+  while(Wire.available() == 0);
+  
+  byte regValue = Wire.read();
+
+  if ((regValue & expectedValue) > 0) {
+  return true;  
   }
-  for (byte i=0;i<3;i++){
-    data[i] = (Wire.read())|(Wire.read()<<8);
+
+  else{
+  return false;
   }
-  Wire.endTransmission();
+  
 }
 
